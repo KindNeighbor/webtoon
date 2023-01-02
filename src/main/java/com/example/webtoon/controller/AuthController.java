c13323cca7537b5591e352f601e830ed7cdd1b37
YJ <zerosix1017@gmail.com>
2023-01-02 16:34:32 +0900

    feat : 로그인 기능 구현

---
+++ src/main/java/com/example/webtoon/controller/AuthController.java
@@ -0,0 +1,26 @@
+package com.example.webtoon.controller;
+
+import com.example.webtoon.payload.LoginRequest;
+import com.example.webtoon.service.AuthService;
+import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+
+    // 로그인
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+        return authService.signIn(loginRequest);
+    }
+}
---
+++ src/main/java/com/example/webtoon/service/AuthService.java
@@ -0,0 +1,35 @@
+package com.example.webtoon.service;
+
+import com.example.webtoon.payload.JwtAuthenticationResponse;
+import com.example.webtoon.payload.LoginRequest;
+import com.example.webtoon.security.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenProvider tokenProvider;
+
+    public ResponseEntity<?> signIn(LoginRequest loginRequest) {
+
+        Authentication authentication = authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(
+                loginRequest.getEmail(),
+                loginRequest.getPassword()
+            )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = tokenProvider.generateToken(authentication);
+        return ResponseEntity.ok(new JwtAuthenticationResponse(jwt));
+    }
+}
